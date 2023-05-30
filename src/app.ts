be803bc7092adc983efb98b15a0e149d0271af4c
euhansarkar <euhansarkar@gmail.com>
2023-05-30 15:28:45 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
-const app: Application = express();
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
 
-app.use(cors());
+              app.use(cors())
 
 // parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+  app.use(express.json())
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+        app.use(express.urlencoded({ extended: true }))
 
-export default app;
+    app.get('/', (req: Request, res: any) => {
+  res.send('Hello World!')
+})
+
+export default app
